dcd0011157b76c6b60273ab1090059e948be7eef
NesliUsta <n01734911@humber.ca>
2026-04-23 15:56:17 -0400

    Test Suits: Checkout - Navigation - Search

---
+++ Test Suites/Search Functionality.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>TS_005 Validate the working of Search functionality</description>
+   <name>Search Functionality</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5b96fc5d-c95e-46e2-a08c-144323e9bd89</testSuiteGuid>
+</TestSuiteEntity>
